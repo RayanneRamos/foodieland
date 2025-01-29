66c00a08507e97f718c56e210bb56d9bb3aaa494
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-29 11:13:46 -0300

    :sparkles: feat: create a card recipes utils

---
+++ src/utils/cardRecipes.ts
@@ -0,0 +1,80 @@
+import cardRecipeImageOne from "../assets/recipes/image-01.png";
+import cardRecipeImageTwo from "../assets/recipes/image-02.png";
+import cardRecipeImageThree from "../assets/recipes/image-03.png";
+import cardRecipeImageFour from "../assets/recipes/image-04.png";
+import cardRecipeImageFive from "../assets/recipes/image-05.png";
+import cardRecipeImageSix from "../assets/reicpes/image-06.png";
+import cardRecipeImageSeven from "../assets/recipes/image-07.png";
+import cardRecipeImageEight from "../assets/recipes/image-08.png";
+import cardRecipeImageNine from "../assets/recipes/image-01.png";
+
+export const cardRecipes = [
+  {
+    id: "1",
+    image: cardRecipeImageOne,
+    favorite: true,
+    name: "Big and Juicy Wagyu Beef Cheeseburger",
+    clock: "30",
+    category: "Snack",
+  },
+  {
+    id: "2",
+    image: cardRecipeImageTwo,
+    favorite: false,
+    name: "Fresh Lime Roasted Salmon With Ginger Sauce",
+    clock: "30",
+    category: "Fish",
+  },
+  {
+    id: "3",
+    image: cardRecipeImageThree,
+    favorite: false,
+    name: "Strawberry Oatmeal Pancake with Honey Syrup",
+    clock: "30",
+    category: "Breakfast",
+  },
+  {
+    id: "4",
+    image: cardRecipeImageFour,
+    favorite: true,
+    name: "Fresh and Healthy Mixed Mayonnaise Salad",
+    clock: "30",
+    category: "Healthy",
+  },
+  {
+    id: "5",
+    image: cardRecipeImageFive,
+    favorite: false,
+    name: "Chicken Meatballs with Cream Cheese",
+    clock: "30",
+    category: "Meat",
+  },
+  {
+    id: "6",
+    image: cardRecipeImageSix,
+  },
+  {
+    id: "7",
+    image: cardRecipeImageSeven,
+    favorite: true,
+    name: "Fruit Pancake with Orange & Blueberry",
+    clock: "30",
+    category: "Sweet",
+  },
+  {
+    id: "8",
+    image: cardRecipeImageEight,
+    favorite: false,
+    name: "The Best Easy One Pot Chicken and Rice",
+    clock: "30",
+    category: "Snack",
+  },
+  {
+    id: "9",
+    image: cardRecipeImageNine,
+    favorite: false,
+    name: "The Creamiest Creamy Chicken and Bacon Pasta",
+    clock: "30",
+    category: "Noodles",
+  },
+];
