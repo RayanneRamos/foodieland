30c2e4abcd9b4804124ff2513b8758747c8740f0
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-29 10:17:23 -0300

    :sparkles: feat: create a categories utils

---
+++ src/utils/categories.ts
@@ -0,0 +1,39 @@
+import breakfastImage from "../assets/categories/breakfast.png";
+import veganImage from "../assets/categories/vegan.png";
+import meatImage from "../assets/categories/meat.png";
+import dessertImage from "../assets/categories/dessert.png";
+import lunchImage from "../assets/categories/lunch.png";
+import chocolateImage from "../assets/categories/chocolate.png";
+
+export const categories = [
+  {
+    id: "1",
+    image: breakfastImage,
+    name: "Breakfast",
+  },
+  {
+    id: "2",
+    image: veganImage,
+    name: "Vegan",
+  },
+  {
+    id: "3",
+    image: meatImage,
+    name: "Meat",
+  },
+  {
+    id: "4",
+    image: dessertImage,
+    name: "Dessert",
+  },
+  {
+    id: "5",
+    image: lunchImage,
+    name: "Lunch",
+  },
+  {
+    id: "6",
+    image: chocolateImage,
+    name: "Chocolate",
+  },
+];
