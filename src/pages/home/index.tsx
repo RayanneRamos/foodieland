b8516fe2934a35b9c45f28f04859ec9de8ca3f63
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-30 10:23:03 -0300

    :sparkles: feat: creata social media section in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,6 +12,11 @@ import { CardRecipes } from "../../components/cardRecipes";
 import { cardRecipes } from "../../utils/cardRecipes";
 import adsImage from "../../assets/recipes/image-06.png";
 import chefImage from '../../assets/chef.png'
+import instagramPostOne from '../../assets/posts/post-01.png'
+import instagramPostTwo from '../../assets/posts/post-02.png'
+import instagramPostThree from '../../assets/posts/post-03.png'
+import instagramPostFour from '../../assets/posts/post-04.png'
+import { InstagramLogo } from "phosphor-react";
 
 export function Home() {
   return (
@@ -84,6 +89,22 @@ export function Home() {
           <button className={styles.chefButton}>Learn More</button>
         </div>
         <img src={chefImage} alt="chef" />
+      </div>
+      <div className={styles.socialContainer}>
+        <div className={styles.socialContent}>
+          <strong className={styles.socialTitle}>Check out @foodieland on Instagram</strong>
+          <span className={styles.socialSubtitle}>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
+          <div className={styles.socialPosts}>
+            <img src={instagramPostOne} alt="post-01" />
+            <img src={instagramPostTwo} alt="post-02" />
+            <img src={instagramPostThree} alt="post-03" />
+            <img src={instagramPostFour} alt="post-04" />
+          </div>
+          <button className={styles.socialButton}>
+            <span className={styles.buttonText}>Visit Our Instagram</span>
+            <InstagramLogo size={24} weight="fill" color="#fff" />
+          </button>
+        </div>
       </div>
     </div>
   );
