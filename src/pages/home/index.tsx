49db783cd1f30f424105af636084f1fc3c990a1d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-30 10:54:41 -0300

    :sparkles: feat: create a more recipes section in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,12 +11,13 @@ import { categories } from "../../utils/categories";
 import { CardRecipes } from "../../components/cardRecipes";
 import { cardRecipes } from "../../utils/cardRecipes";
 import adsImage from "../../assets/recipes/image-06.png";
-import chefImage from '../../assets/chef.png'
-import instagramPostOne from '../../assets/posts/post-01.png'
-import instagramPostTwo from '../../assets/posts/post-02.png'
-import instagramPostThree from '../../assets/posts/post-03.png'
-import instagramPostFour from '../../assets/posts/post-04.png'
+import chefImage from "../../assets/chef.png";
+import instagramPostOne from "../../assets/posts/post-01.png";
+import instagramPostTwo from "../../assets/posts/post-02.png";
+import instagramPostThree from "../../assets/posts/post-03.png";
+import instagramPostFour from "../../assets/posts/post-04.png";
 import { InstagramLogo } from "phosphor-react";
+import { CardOtherRecipes } from "../../components/cardOtherRecipes";
 
 export function Home() {
   return (
@@ -84,16 +85,26 @@ export function Home() {
       </div>
       <div className={styles.chefContainer}>
         <div className={styles.chefContent}>
-          <strong className={styles.chefTitle}>Everyone can be a chef in their own kitchen</strong>
-          <span className={styles.chefSubtitle}>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
+          <strong className={styles.chefTitle}>
+            Everyone can be a chef in their own kitchen
+          </strong>
+          <span className={styles.chefSubtitle}>
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+          </span>
           <button className={styles.chefButton}>Learn More</button>
         </div>
         <img src={chefImage} alt="chef" />
       </div>
       <div className={styles.socialContainer}>
         <div className={styles.socialContent}>
-          <strong className={styles.socialTitle}>Check out @foodieland on Instagram</strong>
-          <span className={styles.socialSubtitle}>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </span>
+          <strong className={styles.socialTitle}>
+            Check out @foodieland on Instagram
+          </strong>
+          <span className={styles.socialSubtitle}>
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+          </span>
           <div className={styles.socialPosts}>
             <img src={instagramPostOne} alt="post-01" />
             <img src={instagramPostTwo} alt="post-02" />
@@ -104,6 +115,27 @@ export function Home() {
             <span className={styles.buttonText}>Visit Our Instagram</span>
             <InstagramLogo size={24} weight="fill" color="#fff" />
           </button>
+        </div>
+      </div>
+      <div className={styles.moreRecipesContainer}>
+        <div className={styles.moreRecipesHeader}>
+          <strong className={styles.moreRecipesHeaderTitle}>
+            Try this delicious recipe to make your day
+          </strong>
+          <span className={styles.moreRecipesHeaderSubtitle}>
+            Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
+          </span>
+        </div>
+        <div className={styles.moreRecipeContent}>
+          <CardOtherRecipes />
+          <CardOtherRecipes />
+          <CardOtherRecipes />
+          <CardOtherRecipes />
+          <CardOtherRecipes />
+          <CardOtherRecipes />
+          <CardOtherRecipes />
+          <CardOtherRecipes />
         </div>
       </div>
     </div>
