731968b56c0a44e7926f45fdd131cca6e9c48741
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-30 11:10:31 -0300

    :sparkles: feat: create a mock more recipes

---
+++ src/utils/moreRecipes.ts
@@ -0,0 +1,75 @@
+import recipesImageTen from "../assets/recipes/image-10.png";
+import recipesImageEleven from "../assets/recipes/image-11.png";
+import recipesImageTwelve from "../assets/recipes/image-12.png";
+import recipesImageThirteen from "../assets/recipes/image-13.png";
+import recipesImageFourteen from "../assets/recipes/image-14.png";
+import recipesImageFiveteen from "../assets/recipes/image-15.png";
+import recipesImageSixteen from "../assets/recipes/image-16.png";
+import recipesImageSeventeen from "../assets/recipes/image-17.png";
+
+export const moreRecipes = [
+  {
+    id: "10",
+    image: recipesImageTen,
+    favorite: true,
+    name: "Mixed Tropical Fruit Salad with Superfood Boosts",
+    clock: "30",
+    category: "Healthy",
+  },
+  {
+    id: "11",
+    image: recipesImageEleven,
+    favorite: false,
+    name: "Big and Juicy Wagyu Beef Cheeseburger",
+    clock: "30",
+    category: "Western",
+  },
+  {
+    id: "12",
+    image: recipesImageTwelve,
+    favorite: true,
+    name: "Healthy Japanese Fried Rice with Asparagus",
+    clock: "30",
+    category: "Healthy",
+  },
+  {
+    id: "13",
+    image: recipesImageThirteen,
+    favorite: false,
+    name: "Cauliflower Walnut Vegetarian Taco Meat",
+    clock: "30",
+    category: "Eastern",
+  },
+  {
+    id: "14",
+    image: recipesImageFourteen,
+    favorite: true,
+    name: "Rainbow Chicken Salad with Almond Honey Mustard Dressing",
+    clock: "30",
+    category: "Healthy",
+  },
+  {
+    id: "15",
+    image: recipesImageFiveteen,
+    favorite: false,
+    name: "Barbeque Spicy Sandwiches with Chips",
+    clock: "30",
+    category: "Snack",
+  },
+  {
+    id: "16",
+    image: recipesImageSixteen,
+    favorite: false,
+    name: "Firecracker Vegan Lettuce Wraps-Spicy!",
+    clock: "30",
+    category: "Seafood",
+  },
+  {
+    id: "17",
+    image: recipesImageSeventeen,
+    favorite: true,
+    name: "Chicken Ramen Soup with Mushroom",
+    clock: "30",
+    category: "Japanese",
+  },
+];
