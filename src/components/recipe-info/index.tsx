7f19b47b7c33ff1550dbd3358ed3ea07d22ede03
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-31 10:28:33 -0300

    :sparkles: feat: making the recipe info component dynamic

--- src/components/recipe-info/index.tsx
+++ src/components/recipe-info/index.tsx
@@ -1,13 +1,18 @@
 import { Clock } from "phosphor-react";
 import styles from "./styles.module.scss";
 
-export function RecipeInfo() {
+interface RecipeInfoProps {
+  title: string;
+  subtitle: string;
+}
+
+export function RecipeInfo({ title, subtitle }: RecipeInfoProps) {
   return (
     <div className={styles.container}>
       <Clock size={24} weight="fill" color="#000" />
       <div className={styles.info}>
-        <span className={styles.title}>Prep time</span>
-        <span className={styles.subtitle}>15 Minutes</span>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.subtitle}>{`${subtitle} Minutes`}</span>
       </div>
     </div>
   );
