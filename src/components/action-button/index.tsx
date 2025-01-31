195598bfae04aca697f40f61320c73319bade142
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-01-31 10:23:54 -0300

    :sparkles: feat: making the action button component dynamic

--- src/components/action-button/index.tsx
+++ src/components/action-button/index.tsx
@@ -1,13 +1,20 @@
-import { Printer } from "phosphor-react";
+import * as Icons from "phosphor-react";
 import styles from "./styles.module.scss";
 
-export function ActionButton() {
+interface ActionButtonProps {
+  icon: keyof typeof Icons;
+  name: string;
+}
+
+export function ActionButton({ icon, name }: ActionButtonProps) {
+  const Icon = Icons[icon] as React.ElementType;
+
   return (
     <div className={styles.container}>
       <button className={styles.button}>
-        <Printer size={24} weight="regular" />
+        <Icon size={24} weight="regular" />
       </button>
-      <span className={styles.name}>Print</span>
+      <span className={styles.name}>{name}</span>
     </div>
   );
 }
