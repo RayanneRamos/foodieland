3062ad37344572c3f00008925481fbf6782b4cab
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 15:27:18 -0300

    :sparkles: feat: adding the task dynamically

--- src/components/directions-task/index.tsx
+++ src/components/directions-task/index.tsx
@@ -1,15 +1,17 @@
 import styles from "./styles.module.scss";
 
-export function DirectionsTask() {
+interface DirectionsTaskProps {
+  task: string;
+}
+
+export function DirectionsTask({ task }: DirectionsTaskProps) {
   return (
     <div className={styles.container}>
       <label className={styles.checkboxContainer}>
         <input type="checkbox" />
         <span className={styles.checkmark}></span>
       </label>
-      <span className={styles.directionTaskName}>
-        1. Lorem ipsum dolor sit amet
-      </span>
+      <span className={styles.directionTaskName}>{task}</span>
     </div>
   );
 }
