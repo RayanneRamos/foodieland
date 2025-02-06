6aa12090efea62adf9241cfc8d5766bacf2a64ae
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 16:30:06 -0300

    :sparkles: feat: create a tasty recipes mock

---
+++ src/utils/tasty-recipes.ts
@@ -0,0 +1,24 @@
+import tastyRecipesImageOne from "../assets/recipes/image-05.png";
+import tastyRecipesImageTwo from "../assets/recipes/image-09.png";
+import tastyRecipesImageThree from "../assets/recipes/image-18.png";
+
+export const tastyRecipes = [
+  {
+    id: "1",
+    image: tastyRecipesImageOne,
+    name: "Chicken Meatballs with Cream Cheese",
+    author: "Andreas Paula",
+  },
+  {
+    id: "2",
+    image: tastyRecipesImageTwo,
+    name: "Traditional Bolognaise Ragu",
+    author: "Andreas Paula",
+  },
+  {
+    id: "3",
+    image: tastyRecipesImageThree,
+    name: "Pork and Chive Chinese Dumplings",
+    author: "Andreas Paula",
+  },
+];
