d966aa695128555ac1b6c74574e80bc8346a3a74
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 15:51:27 -0300

    :sparkles: feat: create a header section in blog list page

--- src/pages/blog-list/index.tsx
+++ src/pages/blog-list/index.tsx
@@ -3,5 +3,26 @@ import { Navigation } from "../../components/navigation";
 import styles from "./styles.module.scss";
 
 export function BlogList() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className={styles.container}>
+      <Navigation />
+      <Divider />
+      <div className={styles.header}>
+        <h1 className={styles.headerTitle}>Blog & Article</h1>
+        <p className={styles.headerSubtitle}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore
+        </p>
+        <div className={styles.form}>
+          <input
+            className={styles.inputEmail}
+            name="email"
+            type="email"
+            placeholder="Your email address..."
+          />
+          <button className={styles.button}>Subscribe</button>
+        </div>
+      </div>
+    </div>
+  );
 }
