9777991ea5e369b43524653a60d9390c45eb7021
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 15:26:34 -0300

    :sparkles: feat: create an other recipes mock

---
+++ src/utils/other-recipe.ts
@@ -0,0 +1,24 @@
+import otherRecipeImageOne from "../assets/recipes/image-05.png";
+import otherRecipeImageTwo from "../assets/recipes/image-09.png";
+import otherRecipeImageThree from "../assets/recipes/image-08.png";
+
+export const otherRecipe = [
+  {
+    id: "1",
+    image: otherRecipeImageOne,
+    name: "Chicken Meatball with Creamy Cheese",
+    author: "Andreas Paula",
+  },
+  {
+    id: "2",
+    image: otherRecipeImageTwo,
+    name: "The Creamiest Creamy Chicken and Bacon Pasta",
+    author: "Andreas Paula",
+  },
+  {
+    id: "3",
+    image: otherRecipeImageThree,
+    name: "The Best Easy One Pot Chicken and Rice",
+    author: "Andreas Paula",
+  },
+];
