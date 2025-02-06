ae5275eb48d23f1d990e3a7c5e7933b8ad6b5d94
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 15:28:04 -0300

    :sparkles: feat: fetching card information dynamically

--- src/components/ingredients-cards/index.tsx
+++ src/components/ingredients-cards/index.tsx
@@ -1,15 +1,21 @@
 import styles from "./styles.module.scss";
-import cardImage from "../../assets/recipes/image-05.png";
 
-export function IngredientsCards() {
+interface IngredientsCardsProps {
+  othersRecipe: {
+    id: string;
+    image: string;
+    name: string;
+    author: string;
+  };
+}
+
+export function IngredientsCards({ othersRecipe }: IngredientsCardsProps) {
   return (
     <div className={styles.container}>
-      <img src={cardImage} alt="" />
+      <img src={othersRecipe.image} alt="" />
       <div className={styles.info}>
-        <strong className={styles.infoTitle}>
-          Chicken Meatball with Creamy Chees...
-        </strong>
-        <span className={styles.infoAuthor}>By Andreas Paula</span>
+        <strong className={styles.infoTitle}>{othersRecipe.name}</strong>
+        <span className={styles.infoAuthor}>By {othersRecipe.author}</span>
       </div>
     </div>
   );
