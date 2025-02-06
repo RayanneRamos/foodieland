6008e60a59840bc64d6827dccc0a2d00c76be13b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 15:08:17 -0300

    :sparkles: feat: creating the function to cross out a completed task

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 
 export function Task() {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className={styles.container}>
       <label className={styles.checkboxContainer}>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
         <span className={styles.checkmark}></span>
       </label>
-      <span className={styles.taskName}>Lorem ipsum dolor sit amet</span>
+      <span className={`${styles.taskName} ${checked ? styles.completed : ""}`}>
+        Lorem ipsum dolor sit amet
+      </span>
     </div>
   );
 }
