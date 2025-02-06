a51464d60147b227ec227c8ad8a950443a278656
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 15:39:58 -0300

    :sparkles: feat: create a blog list page

---
+++ src/pages/blog-list/index.tsx
@@ -0,0 +1,7 @@
+import { Divider } from "../../components/divider";
+import { Navigation } from "../../components/navigation";
+import styles from "./styles.module.scss";
+
+export function BlogList() {
+  return <h1>Hello World</h1>;
+}
