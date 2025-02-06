fef464c712dfdd1786d25437ee183e6c98d0f0d7
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-06 14:43:38 -0300

    :sparkles: feat: create a like recipes mock

---
+++ src/utils/like-recipes.ts
@@ -0,0 +1,39 @@
+import recipesImageTen from "../assets/recipes/image-10.png";
+import recipesImageEleven from "../assets/recipes/image-11.png";
+import recipesImageTwelve from "../assets/recipes/image-12.png";
+import recipesImageThirteen from "../assets/recipes/image-13.png";
+
+export const likeRecipes = [
+  {
+    id: "1",
+    image: recipesImageTen,
+    favorite: true,
+    name: "Mixed Tropical Fruit Salad with Superfood Boosts",
+    clock: "30",
+    category: "Healthy",
+  },
+  {
+    id: "2",
+    image: recipesImageEleven,
+    favorite: false,
+    name: "Big and Juicy Wagyu Beef Cheeseburger",
+    clock: "30",
+    category: "Western",
+  },
+  {
+    id: "3",
+    image: recipesImageTwelve,
+    favorite: true,
+    name: "Healthy Japanese Fried Rice with Asparagus",
+    clock: "30",
+    category: "Healthy",
+  },
+  {
+    id: "4",
+    image: recipesImageThirteen,
+    favorite: false,
+    name: "Cauliflower Walnut Vegetarian Taco Meat",
+    clock: "30",
+    category: "Eastern",
+  },
+];
