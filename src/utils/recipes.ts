285f0e66053ff4ecfb5f954605c66a26bb7cfc87
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-07 15:21:04 -0300

    :sparkles: feat: create a recipes mock

---
+++ src/utils/recipes.ts
@@ -0,0 +1,87 @@
+import recipeImageOne from "../assets/recipes/image-17.png";
+import recipeImageTwo from "../assets/recipes/image-10.png";
+import recipeImageThree from "../assets/recipes/image-19.png";
+import recipeImageFour from "../assets/recipes/image-20.png";
+import recipeImageFive from "../assets/recipes/image-21.png";
+import recipeImageSix from "../assets/recipes/image-22.png";
+import authorImageOne from "../assets/authors/author-01.png";
+import authorImageTwo from "../assets/authors/author-02.png";
+import authorImageThree from "../assets/authors/author-03.png";
+import authorImageFour from "../assets/authors/author-04.png";
+import authorImageFive from "../assets/authors/author-05.png";
+import authorImageSix from "../assets/authors/author-06.png";
+
+export const recipes = [
+  {
+    id: "1",
+    image: recipeImageOne,
+    recipeTitle: "Crochet Projects for Noodles Lovers",
+    recipeDescription:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    author: {
+      image: authorImageOne,
+      name: "Wade Warren",
+    },
+    date: "12 November 2021",
+  },
+  {
+    id: "2",
+    image: recipeImageTwo,
+    recipeTitle: "10 Vegetarian Recipes To Eat This Month",
+    recipeDescription:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    author: {
+      image: authorImageTwo,
+      name: "Robert Fox",
+    },
+    date: "12 November 2021",
+  },
+  {
+    id: "3",
+    image: recipeImageThree,
+    recipeTitle: "Full Guide to Becoming a Professional Chef",
+    recipeDescription:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    author: {
+      image: authorImageThree,
+      name: "Dianne Russell",
+    },
+    date: "12 November 2021",
+  },
+  {
+    id: "4",
+    image: recipeImageFour,
+    recipeTitle: "Simple & Delicious Vegetarian Lasagna",
+    recipeDescription:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    author: {
+      image: authorImageFour,
+      name: "Leslie Alexander",
+    },
+    date: "12 November 2021",
+  },
+  {
+    id: "5",
+    image: recipeImageFive,
+    recipeTitle: "Plantain and Pinto Stew with Aji Verde",
+    recipeDescription:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    author: {
+      image: authorImageFive,
+      name: "Courtney Henry",
+    },
+    date: "12 November 2021",
+  },
+  {
+    id: "6",
+    image: recipeImageSix,
+    recipeTitle: "We're Hiring a Communications Assistant!",
+    recipeDescription:
+      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    author: {
+      image: authorImageSix,
+      name: "Albert Flores",
+    },
+    date: "12 November 2021",
+  },
+];
