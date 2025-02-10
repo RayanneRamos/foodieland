d19d84975dc02e1cadca4b471c6e62c21052c1ad
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-10 09:24:43 -0300

    :sparkles: feat: create a header section in blog post page

--- src/pages/blog-post/index.tsx
+++ src/pages/blog-post/index.tsx
@@ -5,8 +5,28 @@ import avatarImage from "../../assets/avatar.png";
 import blogPostImage from "../../assets/blog-post-image.png";
 export function BlogPost() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.container}>
+      <Navigation />
+      <Divider />
+      <div className={styles.main}>
+        <h1 className={styles.title}>
+          Full Guide to Becoming a Professional Chef
+        </h1>
+        <div className={styles.headerInfo}>
+          <div className={styles.headerAvatar}>
+            <img src={avatarImage} alt="" className={styles.avatarImage} />
+            <span className={styles.avatarName}>John Smith</span>
+          </div>
+          <div className={styles.separator} />
+          <span className={styles.date}>15 March 2022</span>
+        </div>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+          ultrices odio. Nulla at congue diam, at dignissim turpis. Ut vehicula
+          sed velit a faucibus. In feugiat vestibulum velit vel pulvinar.
+        </p>
+        <img src={blogPostImage} alt="" className={styles.imagePost} />
+      </div>
     </div>
   );
 }
