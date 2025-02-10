2e9a75ff0582799f2f21d52a6035ce10a66d521a
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-10 09:24:01 -0300

    :sparkles: feat: create a blog post page

---
+++ src/pages/blog-post/index.tsx
@@ -0,0 +1,12 @@
+import { Divider } from "../../components/divider";
+import { Navigation } from "../../components/navigation";
+import styles from "./styles.module.scss";
+import avatarImage from "../../assets/avatar.png";
+import blogPostImage from "../../assets/blog-post-image.png";
+export function BlogPost() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
