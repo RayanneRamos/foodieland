0f5c5ee49433c6b6383d5f1377036b772a795114
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-12 10:13:15 -0300

    :sparkles: feat: create a recipes section in recipes page

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -1,9 +1,31 @@
+import { CardOtherRecipes } from "../../components/card-other-recipes";
+import { Divider } from "../../components/divider";
+import { Navigation } from "../../components/navigation";
+import { likeRecipes } from "../../utils/like-recipes";
 import styles from "./styles.module.scss";
 
 export function Recipes() {
   return (
     <div className={styles.container}>
-      <h1>Recipes</h1>
+      <Navigation />
+      <Divider />
+      <div className={styles.main}>
+        <h1 className={styles.title}>Recipes Lists</h1>
+        <div className={styles.recipesContainer}>
+          {likeRecipes.map((recipe) => {
+            return <CardOtherRecipes moreRecipe={recipe} key={recipe.id} />;
+          })}
+          {likeRecipes.map((recipe) => {
+            return <CardOtherRecipes moreRecipe={recipe} key={recipe.id} />;
+          })}
+          {likeRecipes.map((recipe) => {
+            return <CardOtherRecipes moreRecipe={recipe} key={recipe.id} />;
+          })}
+          {likeRecipes.map((recipe) => {
+            return <CardOtherRecipes moreRecipe={recipe} key={recipe.id} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
