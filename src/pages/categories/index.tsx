f5ee662dabc99d287de4bd7543935e50df11135d
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-12 16:33:55 -0300

    :sparkles: feat: create a category section in categories page

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,9 +1,28 @@
+import { Category } from "../../components/category";
+import { Divider } from "../../components/divider";
+import { Navigation } from "../../components/navigation";
+import { categories } from "../../utils/categories";
 import styles from "./styles.module.scss";
 
 export function Categories() {
   return (
     <div className={styles.container}>
-      <h1>Categories</h1>
+      <Navigation />
+      <Divider />
+      <div className={styles.main}>
+        <h1 className={styles.title}>Categories</h1>
+        <div className={styles.categoriesContainer}>
+          {categories.map((category) => {
+            return (
+              <Category
+                image={category.image}
+                name={category.name}
+                key={category.id}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
