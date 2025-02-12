fcc87dbed3211196d95cb37456d20e598ab29c8c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-12 16:19:50 -0300

    :sparkles: feat: add more categories

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -4,6 +4,18 @@ import meatImage from "../assets/categories/meat.png";
 import dessertImage from "../assets/categories/dessert.png";
 import lunchImage from "../assets/categories/lunch.png";
 import chocolateImage from "../assets/categories/chocolate.png";
+import chickenImage from "../assets/categories/chicken.png";
+import fishImage from "../assets/categories/fish.png";
+import saladImage from "../assets/categories/salad.png";
+import soupImage from "../assets/categories/soup.png";
+import pastaImage from "../assets/categories/pasta.png";
+import beveragesImage from "../assets/categories/beverages.png";
+import healthyImage from "../assets/categories/healthy.png";
+import koreanImage from "../assets/categories/korean.png";
+import thailandImage from "../assets/categories/thailand.png";
+import japanImage from "../assets/categories/japan.png";
+import chinaImage from "../assets/categories/china.png";
+import airfryerImage from "../assets/categories/air-fryer.png";
 
 export const categories = [
   {
@@ -35,5 +47,65 @@ export const categories = [
     id: "6",
     image: chocolateImage,
     name: "Chocolate",
+  },
+  {
+    id: "7",
+    image: chickenImage,
+    name: "Chicken",
+  },
+  {
+    id: "8",
+    image: fishImage,
+    name: "Seafood",
+  },
+  {
+    id: "9",
+    image: saladImage,
+    name: "Salad",
+  },
+  {
+    id: "10",
+    image: soupImage,
+    name: "Soup",
+  },
+  {
+    id: "11",
+    image: pastaImage,
+    name: "Pasta",
+  },
+  {
+    id: "12",
+    image: beveragesImage,
+    name: "Beverages",
+  },
+  {
+    id: "13",
+    image: healthyImage,
+    name: "Healthy",
+  },
+  {
+    id: "14",
+    image: koreanImage,
+    name: "Korean",
+  },
+  {
+    id: "15",
+    image: thailandImage,
+    name: "Thailand",
+  },
+  {
+    id: "16",
+    image: japanImage,
+    name: "Japan",
+  },
+  {
+    id: "17",
+    image: chinaImage,
+    name: "China",
+  },
+  {
+    id: "18",
+    image: airfryerImage,
+    name: "Air Fryer",
   },
 ];
