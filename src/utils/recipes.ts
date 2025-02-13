69d8978e4f448b67e7a209d19a0858276e3602e9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-13 16:28:03 -0300

    :sparkles: feat: create a recipes breakfast mock

--- src/utils/recipes.ts
+++ src/utils/recipes.ts
@@ -1,87 +1,236 @@
-import recipeImageOne from "../assets/recipes/image-17.png";
-import recipeImageTwo from "../assets/recipes/image-10.png";
-import recipeImageThree from "../assets/recipes/image-19.png";
-import recipeImageFour from "../assets/recipes/image-20.png";
-import recipeImageFive from "../assets/recipes/image-21.png";
-import recipeImageSix from "../assets/recipes/image-22.png";
-import authorImageOne from "../assets/authors/author-01.png";
-import authorImageTwo from "../assets/authors/author-02.png";
-import authorImageThree from "../assets/authors/author-03.png";
-import authorImageFour from "../assets/authors/author-04.png";
-import authorImageFive from "../assets/authors/author-05.png";
-import authorImageSix from "../assets/authors/author-06.png";
-
-export const recipes = [
+export const recipe = [
+  // Breakfast
   {
-    id: "1",
-    image: recipeImageOne,
-    recipeTitle: "Crochet Projects for Noodles Lovers",
-    recipeDescription:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    id: "b92f16a8-8829-48d7-bb12-975e5a8eeb89",
+    recipeTitle: "Avocado Toast with Poached Egg",
     author: {
-      image: authorImageOne,
-      name: "Wade Warren",
+      authorAvatar: "",
+      authorName: "Emily Clark",
+      datePosted: "14 January 2024",
     },
-    date: "12 November 2021",
+    prepareTime: "5",
+    cookTime: "10",
+    recipeCategory: "Breakfast",
+    recipeImage: "",
+    nutrition: {
+      calories: "320",
+      totalFat: "20",
+      protein: "12",
+      carbohydrate: "30",
+      cholesterol: "210",
+    },
+    recipeDescription:
+      "A simple yet delicious avocado toast topped with a perfectly poached egg. The creamy avocado pairs wonderfully with the rich flavor of the egg. It's a perfect breakfast to start your day. Quick and nutritious, it provides the energy you need.",
+    recipeIngredients: [
+      {
+        recipeSteps: [
+          {
+            name: "For the toast",
+            steps: [
+              {
+                ingredientsQuantity: "2",
+                ingredientsName: "slices whole grain bread",
+              },
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "ripe avocado",
+              },
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "tbsp olive oil",
+              },
+            ],
+          },
+          {
+            name: "For the poached egg",
+            steps: [
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "egg",
+              },
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "tbsp white vinegar",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    recipeDirections: [
+      {
+        directionTitle: "Toast the bread",
+        directionDescription:
+          "Toast the slices of whole grain bread and drizzle with olive oil.",
+      },
+      {
+        directionTitle: "Prepare the poached egg",
+        directionDescription:
+          "Poach the egg in simmering water with a splash of vinegar until the whites are set and yolk is runny.",
+      },
+      {
+        directionTitle: "Assemble the toast",
+        directionDescription:
+          "Mash the avocado and spread it evenly on the toasted bread. Top with the poached egg.",
+      },
+      {
+        directionTitle: "Serve",
+        directionDescription:
+          "Season with salt and pepper, and enjoy your healthy and satisfying breakfast.",
+      },
+    ],
+    directionImage: "",
   },
   {
-    id: "2",
-    image: recipeImageTwo,
-    recipeTitle: "10 Vegetarian Recipes To Eat This Month",
-    recipeDescription:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    id: "347bc8e3-c2f4-4c75-922b-5673f6b3f118",
+    recipeTitle: "Banana Pancakes",
     author: {
-      image: authorImageTwo,
-      name: "Robert Fox",
+      authorAvatar: "",
+      authorName: "James Lee",
+      datePosted: "10 February 2024",
     },
-    date: "12 November 2021",
+    prepareTime: "5",
+    cookTime: "15",
+    recipeCategory: "Breakfast",
+    recipeImage: "",
+    nutrition: {
+      calories: "210",
+      totalFat: "8",
+      protein: "4",
+      carbohydrate: "30",
+      cholesterol: "35",
+    },
+    recipeDescription:
+      "Fluffy banana pancakes with a subtle sweetness from ripe bananas. These pancakes are soft and easy to make, perfect for a quick breakfast. The banana flavor is rich and delicious, making them a favorite. Serve with syrup, berries, or nuts for extra flavor.",
+    recipeIngredients: [
+      {
+        recipeSteps: [
+          {
+            name: "For the pancake batter",
+            steps: [
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "ripe banana",
+              },
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "egg",
+              },
+              {
+                ingredientsQuantity: "1/2",
+                ingredientsName: "cup flour",
+              },
+              {
+                ingredientsQuantity: "1/4",
+                ingredientsName: "cup almond milk",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    recipeDirections: [
+      {
+        directionTitle: "Mash the banana",
+        directionDescription:
+          "Mash the ripe banana in a bowl until smooth, then add the egg and almond milk.",
+      },
+      {
+        directionTitle: "Prepare the pancake batter",
+        directionDescription:
+          "Mix the flour into the banana mixture and stir until a smooth batter forms.",
+      },
+      {
+        directionTitle: "Cook the pancakes",
+        directionDescription:
+          "Pour spoonfuls of the batter onto a heated, greased pan. Cook until bubbles form on the surface, then flip and cook until golden brown.",
+      },
+      {
+        directionTitle: "Serve",
+        directionDescription:
+          "Serve the pancakes hot with syrup, berries, or nuts for added flavor.",
+      },
+    ],
+    directionImage: "",
   },
   {
-    id: "3",
-    image: recipeImageThree,
-    recipeTitle: "Full Guide to Becoming a Professional Chef",
-    recipeDescription:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
+    id: "9b862ed6-b24e-42e4-b8cf-0840e80d78c2",
+    recipeTitle: "Vegan Chia Pudding",
     author: {
-      image: authorImageThree,
-      name: "Dianne Russell",
+      authorAvatar: "",
+      authorName: "Sophia Brown",
+      datePosted: "02 March 2024",
     },
-    date: "12 November 2021",
-  },
-  {
-    id: "4",
-    image: recipeImageFour,
-    recipeTitle: "Simple & Delicious Vegetarian Lasagna",
+    prepareTime: "5",
+    cookTime: "0",
+    recipeCategory: "Breakfast",
+    recipeImage: "",
+    nutrition: {
+      calories: "250",
+      totalFat: "15",
+      protein: "5",
+      carbohydrate: "30",
+      cholesterol: "0",
+    },
     recipeDescription:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    author: {
-      image: authorImageFour,
-      name: "Leslie Alexander",
-    },
-    date: "12 November 2021",
+      "A creamy and nutritious chia pudding made with coconut milk. It's a great vegan option for a filling breakfast. The chia seeds provide healthy fats and fiber, while the coconut milk adds a rich taste. Top with your favorite fruits or nuts for extra texture.",
+    recipeIngredients: [
+      {
+        recipeSteps: [
+          {
+            name: "For the pudding base",
+            steps: [
+              {
+                ingredientsQuantity: "3",
+                ingredientsName: "tbsp chia seeds",
+              },
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "cup coconut milk",
+              },
+              {
+                ingredientsQuantity: "1",
+                ingredientsName: "tbsp maple syrup",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    recipeDirections: [
+      {
+        directionTitle: "Mix the pudding ingredients",
+        directionDescription:
+          "Combine chia seeds, coconut milk, and maple syrup in a bowl. Stir well to combine.",
+      },
+      {
+        directionTitle: "Refrigerate",
+        directionDescription:
+          "Cover the bowl and refrigerate for at least 4 hours or overnight until the chia seeds have absorbed the liquid and the pudding thickens.",
+      },
+      {
+        directionTitle: "Serve",
+        directionDescription:
+          "Top with your favorite fruits, nuts, or granola for a complete breakfast.",
+      },
+    ],
+    directionImage: "",
   },
-  {
-    id: "5",
-    image: recipeImageFive,
-    recipeTitle: "Plantain and Pinto Stew with Aji Verde",
-    recipeDescription:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    author: {
-      image: authorImageFive,
-      name: "Courtney Henry",
-    },
-    date: "12 November 2021",
-  },
-  {
-    id: "6",
-    image: recipeImageSix,
-    recipeTitle: "We're Hiring a Communications Assistant!",
-    recipeDescription:
-      "Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ",
-    author: {
-      image: authorImageSix,
-      name: "Albert Flores",
-    },
-    date: "12 November 2021",
-  },
+  // Vegan
+  // Meat
+  // Dessert
+  // Lunch
+  // Chocolate
+  // Chicken
+  // Seafood
+  // Salad
+  // Soup
+  // Pasta
+  // Beverages
+  // Healthy
+  // Korean
+  // Thailand
+  // Japan
+  // China
+  // Air Fryer
 ];
