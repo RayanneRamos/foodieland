63834f700578552a76bcba90a394b498f33808a7
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-14 19:03:48 -0300

    :sparkles: feat: extend the interface in category component

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,13 +1,14 @@
+import { ButtonHTMLAttributes } from "react";
 import styles from "./styles.module.scss";
 
-interface CategoryProps {
+interface CategoryProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   image: string;
   name: string;
 }
 
-export function Category({ image, name }: CategoryProps) {
+export function Category({ image, name, ...props }: CategoryProps) {
   return (
-    <button className={styles.container}>
+    <button className={styles.container} {...props}>
       <img src={image} alt="" />
       <span className={styles.name}>{name}</span>
     </button>
