ef39e67ff625c6ff493e0142aeadb3b4278b961b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-14 19:02:59 -0300

    :sparkles: feat: extend the interface

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,16 +1,17 @@
+import { ButtonHTMLAttributes } from "react";
 import styles from "./styles.module.scss";
-import * as Icons from 'phosphor-react'
+import * as Icons from "phosphor-react";
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: keyof typeof Icons;
-  name: string
+  name: string;
 }
 
-export function Button({ icon, name }: ButtonProps) {
-  const Icon = icon ? Icons[icon] as React.ElementType : null;
+export function Button({ icon, name, ...props }: ButtonProps) {
+  const Icon = icon ? (Icons[icon] as React.ElementType) : null;
 
   return (
-    <button className={styles.container}>
+    <button className={styles.container} {...props}>
       <span className={styles.name}>{name}</span>
       {Icon && <Icon size={24} weight="fill" />}
     </button>
