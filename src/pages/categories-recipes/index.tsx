08df38b7d40bc7dde73771ba2605ca829eebefd8
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-02-21 14:34:16 -0300

    :sparkles: feat: recipes filtered

--- src/pages/categories-recipes/index.tsx
+++ src/pages/categories-recipes/index.tsx
@@ -5,18 +5,27 @@ import { Navigation } from "../../components/navigation";
 import { Newsletter } from "../../components/newsletter";
 import styles from "./styles.module.scss";
 import { categories } from "../../utils/categories";
+import { recipes } from "../../utils/recipes";
+import { CardOtherRecipes } from "../../components/card-other-recipes";
 
 export function CategoriesRecipes() {
   const { categoryId } = useParams();
   const category = categories.find((category) => category.id === categoryId);
+  const filteredRecipes = recipes.filter(
+    (recipe) => recipe.categoryId === categoryId
+  );
 
   return (
     <div className={styles.container}>
       <Navigation />
       <Divider />
       <div className={styles.main}>
-        <h1>{category?.name} Recipes</h1>
-        <div className={styles.recipesContainer}></div>
+        <h1 className={styles.title}>{category?.name} Recipes</h1>
+        <div className={styles.recipesContainer}>
+          {filteredRecipes.map((recipe) => {
+            return <CardOtherRecipes moreRecipe={recipe} key={recipe.id} />;
+          })}
+        </div>
         <div className={styles.newsletterSection}>
           <Newsletter />
         </div>
