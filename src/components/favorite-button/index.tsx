2a44f34e23fa31271e57a014eeb90723014cd68e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-04-04 23:07:59 -0300

    :label: create a favorite button interface

--- src/components/favorite-button/index.tsx
+++ src/components/favorite-button/index.tsx
@@ -1,10 +1,27 @@
 import { Heart } from "phosphor-react";
 import styles from "./styles.module.scss";
+import { ButtonHTMLAttributes } from "react";
 
-export function FavoriteButton() {
+interface FavoriteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isFavorite: boolean;
+  toggleFavorite: () => void;
+}
+
+export function FavoriteButton({
+  isFavorite,
+  toggleFavorite,
+  ...props
+}: FavoriteButtonProps) {
   return (
-    <button className={styles.container}>
-      <Heart size={24} weight="fill" />
+    <button
+      className={styles.container}
+      onClick={(event) => {
+        event.preventDefault();
+        toggleFavorite();
+      }}
+      {...props}
+    >
+      <Heart size={24} weight="fill" color={isFavorite ? "#ff6363" : "#ccc"} />
     </button>
   );
 }
