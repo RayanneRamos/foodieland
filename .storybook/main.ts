ebb186e784972f97a5bd2f123f630025976c763b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-04-07 21:15:24 -0300

    :wrench: chore: settings storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,30 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  async viteFinal(config) {
+    // só pra garantir que vai processar SCSS
+    return {
+      ...config,
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: "", // você pode usar pra injetar variáveis globais
+          },
+        },
+      },
+    };
+  },
 };
+
 export default config;
