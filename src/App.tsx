43ee7a73fc2d68f95dfcb66b904135aa28a74864
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-09-18 14:09:15 -0300

    :wrench: chore: setting query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Toaster } from "sonner";
 import { Router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster position="bottom-right" richColors />
       <Router />
-    </>
+    </QueryClientProvider>
   );
 }
