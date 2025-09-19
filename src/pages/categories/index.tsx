47f03d730ab86bda71d2806198c357c02d1f340b
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-09-19 14:49:15 -0300

    :sparkles: feat: get category data coming from the api in page categories

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -4,13 +4,33 @@ import { Divider } from "../../components/divider";
 import { Footer } from "../../components/footer";
 import { Navigation } from "../../components/navigation";
 import { Newsletter } from "../../components/newsletter";
-import { categories } from "../../utils/categories";
 import styles from "./styles.module.scss";
 import * as motion from "motion/react-client";
 import { Title } from "../../components/title";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Categories as typeCategories,
+  fetchCategories,
+} from "../../services/fetch-categories";
 
 export function Categories() {
   const navigate = useNavigate();
+  const {
+    data: categories,
+    isLoading,
+    error,
+  } = useQuery<typeCategories[]>({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+
+  if (isLoading) {
+    return <p>Carregando...</p>;
+  }
+
+  if (error instanceof Error) {
+    return <p>Erro: {error.message}</p>;
+  }
 
   return (
     <div className={styles.container}>
@@ -26,11 +46,11 @@ export function Categories() {
           <Title>Categories</Title>
         </motion.h1>
         <div className={styles.categoriesContainer}>
-          {categories.map((category) => {
+          {categories?.map((category) => {
             return (
               <Category
-                image={category.image}
-                name={category.name}
+                image={`http://localhost:3333${category.categoryImage}`}
+                name={category.categoryName}
                 key={category.id}
                 onClick={() => navigate(`/categories/${category.id}`)}
               />
