5156aa9f640a374d4ee81e951632200e2feffb35
Rayanne Ramos <rayanneramosoficial@gmail.com>
2025-09-26 14:37:05 -0300

    :bug: fix: add a role in loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -3,9 +3,10 @@ import { motion } from "motion/react";
 
 export function Loading() {
   return (
-    <div className={styles.loadingContainer}>
+    <div className={styles.loadingContainer} role="status">
       <motion.div
         className={styles.loadingCircle}
+        data-testid="loading-circle"
         animate={{ scale: [1, 1.5, 1], opacity: [0.5, 1, 0.5] }}
         transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
       />
